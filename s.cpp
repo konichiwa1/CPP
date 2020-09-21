86f49314213a02075c82e33a0eab9ebe0aab6ace
konichiwa1 <atul01022001@gmail.com>
2020-09-21 11:38:33 +0530

    git commit -m "initial commit"

---
+++ s.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+#define ll long long 
+#define MOD (ll)(1e9+7)
+#define INF INT32_MAX
+#define INFL INT64_MAX
+
+void solve()    {
+
+}
+
+int main()  {
+    int t = 1;
+    cin >> t;
+    for(int i=0; i<=t; i++) solve();
+    return 0;
+}
